b90b12af8826b0b200fa9a9b3f8d691c91327fdf
ozankahramanoglu1 <ozankahramanoglu96@gmail.com>
2019-12-12 01:45:03 +0300

    New line parsin is not finished, but merged to master

--- demo.py
+++ demo.py
@@ -1,3 +1,16 @@
+class dateAnnotedMessage:
+    date = ''
+    hour = ''
+    messageText = 'text'
+    def __init__(self, date ,messageHour,messageText):
+        #messageArray[0] = time that message sent
+        self.date = date
+        self.hour = messageHour
+        self.messageText = messageText
+
+    def __str__(self):
+        print("date of message: " + self.date + "\thour of message : " + self.hour + "\ttext of message : " + self.messageText)
+
 
 def removeNewLines(fileName):
     File = open(fileName, "r", encoding="utf8")
@@ -5,15 +18,13 @@ def removeNewLines(fileName):
     allMessages = []
     stillSameMessage = ''
     for line in File:
-        if ('-' not in line) and (':' not in line) and (line.count('/') != 2):
-            stillSameMessage += line
+        if (('-' not in line) and (':' not in line)) or ('http' in line):
+            stillSameMessage += line.replace('\n', ' ').replace('-', ' ').replace(':', ' ').replace('\n', ' ')
         else:
-            allMessages.append(line.replace('\n',' '))
+            allMessages.append(line.replace('\n', ' '))
             stillSameMessage = ''
 
     File.close()
-
-    f = open("demofile2.txt", "a", encoding="utf8")
 
     for i in allMessages:
         if i.count('\n') > 0:
@@ -22,4 +33,25 @@ def removeNewLines(fileName):
     return allMessages
 
 
+def categoryByDate(messageArray):
+    alldates = []
+    for message in messageArray:
+        dateAndMessage = message.split('-')
+        fullDate = dateAndMessage[0].split(' ')
+        date = dateAndMessage[0]
+        time = fullDate[1:]
+        message = dateAndMessage[1:]
+        alldates.append(date)
+        # else:
+        #     alldates[alldates.index(date)].append(dateAnnotedMessage(date, time, message))
+    alldates = set(alldates)
+    return alldates
+
 newLinesRemoved = removeNewLines("bestDataEver.txt")
+for i in newLinesRemoved:
+    print(i)
+
+categorizedByDateList = categoryByDate(newLinesRemoved)
+
+for i in categorizedByDateList:
+    print(i)
