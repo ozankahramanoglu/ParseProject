751c2adb18b58fa1a3ece375ff42c88fc83c7b07
ozankahramanoglu1 <ozankahramanoglu96@gmail.com>
2019-12-12 22:37:16 +0300

    No new line Problem, Also Json output categorized by date

--- demo.py
+++ demo.py
@@ -1,27 +1,38 @@
+import re
+import numpy
+
 class dateAnnotedMessage:
     date = ''
     hour = ''
-    messageText = 'text'
+    messageText = ''
     def __init__(self, date ,messageHour,messageText):
-        #messageArray[0] = time that message sent
+        """Every message know date, hour and text of its own"""
         self.date = date
         self.hour = messageHour
         self.messageText = messageText
 
     def __str__(self):
-        print("date of message: " + self.date + "\thour of message : " + self.hour + "\ttext of message : " + self.messageText)
+        return "date of message: " + self.date + "\thour of message : " + self.hour + "\ttext of message : " + self.messageText
+
+    def __dict__(self):
+        return {"date": self.date,
+                "hour": self.hour,
+                "message": self.messageText}
 
 
 def removeNewLines(fileName):
+    """Clean the code from new lined messages"""
     File = open(fileName, "r", encoding="utf8")
 
     allMessages = []
     stillSameMessage = ''
     for line in File:
-        if (('-' not in line) and (':' not in line)) or ('http' in line):
-            stillSameMessage += line.replace('\n', ' ').replace('-', ' ').replace(':', ' ').replace('\n', ' ')
+        if not re.match("[0-3][0-9].[0-1][0-9].20[0-9][0-9]\\s[0-2][0-9]:[0-5][0-9]\\s-.*.", line):
+            stillSameMessage += line.replace('\n', ' ').replace('-', '').replace(':', '').replace('\\', '').replace('"', '').rstrip('\n')
+            #stillSameMessage += re.sub(r'[^A-Za-z0-9 -.ğĞüÜİıŞşÖöÇç:,><!+%()[\]{}]+', ' ', line)
         else:
-            allMessages.append(line.replace('\n', ' '))
+            allMessages.append(line.replace('\n', ' ').replace('\\', '').replace('"', '').rstrip('\n') + stillSameMessage)
+            #allMessages.append(re.sub(r'[^A-Za-z0-9 -.ğĞüÜİıŞşÖöÇç:,><!+%()[\]{}]+', ' ', line) + stillSameMessage)
             stillSameMessage = ''
 
     File.close()
@@ -34,24 +45,56 @@ def removeNewLines(fileName):
 
 
 def categoryByDate(messageArray):
-    alldates = []
+    """Categorize the messages based on date"""
+    alldates = {}
     for message in messageArray:
         dateAndMessage = message.split('-')
         fullDate = dateAndMessage[0].split(' ')
-        date = dateAndMessage[0]
-        time = fullDate[1:]
-        message = dateAndMessage[1:]
-        alldates.append(date)
-        # else:
-        #     alldates[alldates.index(date)].append(dateAnnotedMessage(date, time, message))
-    alldates = set(alldates)
+        date = fullDate[0]
+
+        time = ''.join([str(elem) for elem in fullDate[1:]])  #timeSTR.join(fullDate[1:])
+
+        message = ''.join([str(elem) for elem in dateAndMessage[1:]]) #messageSTR.join(dateAndMessage[1:])
+
+        if date not in alldates:
+            messageArr =[]
+            messageArr.append(dateAnnotedMessage(date, time, message))
+            alldates[date] = messageArr
+        else:
+            appendedMessages = alldates.get(date)
+            appendedMessages.append(dateAnnotedMessage(date, time, message))
+            alldates[date] = appendedMessages
     return alldates
 
-newLinesRemoved = removeNewLines("bestDataEver.txt")
-for i in newLinesRemoved:
-    print(i)
 
-categorizedByDateList = categoryByDate(newLinesRemoved)
+if __name__ == "__main__":
+    newLinesRemoved = removeNewLines("bestDataEver.txt")
+    categorizedByDateList = categoryByDate(newLinesRemoved)
 
-for i in categorizedByDateList:
-    print(i)
+    # for i in categorizedByDateList:
+    #     for k in categorizedByDateList[i]:
+    #         print(k)
+
+    f = open("result.json", "a+", encoding="utf8")
+    f.writelines("{" + '\n')
+    otherCount = 0
+    for date in categorizedByDateList:
+        otherCount += 1
+        f.writelines("\t\"" + date + "\": [" + '\n')
+        count = 0
+        for messages in categorizedByDateList[date]:
+            count += 1
+            f.writelines("\t\t{" + '\n')
+            f.writelines("\t\t\t\"date\": \"" + messages.date + "\"," + '\n')
+            f.writelines("\t\t\t\"hour\": \"" + messages.hour + "\"," + '\n')
+            f.writelines("\t\t\t\"message\": \"" + messages.messageText + "\"" + '\n')
+            if count == len(categorizedByDateList[date]):
+                f.writelines("\t\t}" + '\n')
+            else:
+                f.writelines("\t\t}," + '\n')
+        if otherCount == len(categorizedByDateList):
+            f.writelines("\t]" + '\n')
+        else:
+            f.writelines("\t]," + '\n')
+    f.writelines("}" + '\n')
+    f.close()
